dd9b9e3c5e6e6499bbc1e4502936bd2873cb628d
Viji-P <mpvijayalakshmi91103@gmail.com>
2023-11-23 12:27:39 +0530

    Full Stack Application

---
+++ springboot-backend/src/test/java/com/springbootApplication/springbootbackend/SpringbootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootApplication.springbootbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
